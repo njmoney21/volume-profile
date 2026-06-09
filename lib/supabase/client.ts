13e38c88ec81defa34f76eb9afe796db89094385
njmoney21 <nikolatodorovic800@gmail.com>
2026-06-09 19:37:50 +0200

    chore: add TODO for Database generic on Supabase clients

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,5 +1,6 @@
 import { createBrowserClient } from '@supabase/ssr'
 
+// TODO: add Database generic after running: npx supabase gen types typescript --project-id <id> > types/database.ts
 export function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
